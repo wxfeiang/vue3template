563691dcde2a51d649aee8f20754906cf0ef4c0d
wxfeiang <wxfeiang@qq.com>
2023-03-19 10:47:44 +0800

    test(custom): 删除导入的文件测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,12 +2,10 @@
  * @Author: wxfeiang
  * @Description:
  * @Date: 2023-02-19 11:57:53
- * @LastEditTime: 2023-02-19 17:47:15
+ * @LastEditTime: 2023-03-19 10:46:46
  * @FilePath: /vue3template/src/components/HelloWorld.vue
 -->
 <script setup lang="ts">
-import { ref } from "vue";
-
 defineProps<{ msg: string }>();
 
 console.log(" HI: ====", import.meta.env.VITE_BASE_API);
